04df8d6655e2637d54492c0c3b4f161e459c061b
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-08-22 22:54:25 -0500

    Agregando usuarios

--- inc/modelos/modelo-admin.php
+++ inc/modelos/modelo-admin.php
@@ -8,10 +8,23 @@ if($accion === 'crear'){
     'cost' => 12
     );
     $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
-    $respuesta = array(
-    'pass' => $hash_password
-    );
-    echo json_encode($respuesta);
+    // importar la conexion
+    include '../funciones/conexion.php';
+    try{//Realizar la consulta a la base de datos
+      $stmt = $con->prepare("Insert into usuarios (usuario, password) Values (?, ?)");
+        $stmt->bind_param('ss', $usuario, $hash_password);
+        $stmt->execute();
+        $respuesta = array(
+    'respuesta' => $stmt->error_list,
+        'error' => $stmt->error );
+        $stmt->close();
+        $con->close();
+    }catch(Exception $e){//En caso de un error, tomar la excepcion
+         $respuesta = array(
+    'pass' => $e->getMessage() );
+    }
+   echo json_encode($respuesta);
+   
 }
 if($accion === 'login'){
     
--- js/formulario.js
+++ js/formulario.js
@@ -1,35 +1,80 @@
+
 eventListeners();
-function eventListeners(){
+
+function eventListeners() {
     document.querySelector('#formulario').addEventListener('submit', validarRegistro);
 }
 
-function validarRegistro(e){
+
+function validarRegistro(e) {
     e.preventDefault();
+    
     var usuario = document.querySelector('#usuario').value,
-    tipo = document.querySelector('#tipo').value,
-    password = document.querySelector('#password').value;
-    if (usuario === '' || password === ''){
-        swal({
-            type: 'error',
-            title: 'Error!',
-            text: 'Ambos campos son obligatorios!'
-        })
-    } else{
-        var datos = new FormData();
-        datos.append('usuario', usuario);
-        datos.append('password', password);
-        datos.append('accion', tipo);
-        //lamado ajax
-        var xhr = new XMLHttpRequest();
-        //abrir la conexion
-        xhr.open('POST', 'inc/modelos/modelo-admin.php', true);
-        //retorno de datos
-        xhr.onload = function(){
-            if(this.status === 200){
-                console.log(JSON.parse(xhr.responseText));
+        password = document.querySelector('#password').value,
+        tipo = document.querySelector('#tipo').value;
+        
+        if(usuario === '' || password === ''){
+            // la validación falló
+            swal({
+              type: 'error',
+              title: 'Error!',
+              text: 'Ambos campos son obligatorios!'
+            })
+        } else {
+            // Ambos campos son correctos, mandar ejecutar Ajax
+            
+            // datos que se envian al servidor
+            var datos = new FormData();
+            datos.append('usuario', usuario);
+            datos.append('password', password);
+            datos.append('accion', tipo);
+            
+            // crear el llamado a ajax
+            var xhr = new XMLHttpRequest();
+            
+            // abrir la conexión.
+            xhr.open('POST', 'inc/modelos/modelo-admin.php', true);
+            
+            // retorno de datos
+            xhr.onload = function(){
+                if(this.status === 200) {
+                    var respuesta = JSON.parse(xhr.responseText);
+                    
+                    console.log(respuesta);
+                    // Si la respuesta es correcta
+                    if(respuesta.respuesta === 'correcto') {
+                        // si es un nuevo usuario
+                        if(respuesta.tipo === 'crear') {
+                            swal({
+                                title: 'Usuario Creado',
+                                text: 'El usuario se creó correctamente',
+                                type: 'success'
+                            });
+                        } else if(respuesta.tipo === 'login'){
+                            swal({
+                                title: 'Login Correcto',
+                                text: 'Presiona OK para abrir el dashboard',
+                                type: 'success'
+                            })
+                            .then(resultado => {
+                                if(resultado.value) {
+                                    window.location.href = 'index.php';
+                                }
+                            })
+                        }
+                    } else {
+                        // Hubo un error
+                        swal({
+                            title: 'Error',
+                            text: 'Hubo un error',
+                            type: 'error'
+                        })
+                    }
+                }
             }
+            
+            // Enviar la petición
+            xhr.send(datos);
+            
         }
-        //enviar la peticion
-        xhr.send(datos);
-    }
 }
